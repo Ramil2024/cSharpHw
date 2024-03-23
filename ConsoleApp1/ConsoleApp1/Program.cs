9114af6be5bfb673b9052b4ac5496a60969fa822
unknown <hajrullinrr@yandex.ru>
2024-03-23 19:39:47 +0300

    coment

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace ConsoleApp1
+//{
+
+//    public class Ramill
+//    {
+
+//        public static void Main(string[] args)
+//        {
+//            string name = "Рамиль";
+//            string family = "Хайруллин";
+//            int Age = 33;
+//            int cheight = 173;
+//            int weight = 85;
+//            Console.WriteLine("Меня зовут " + name + ". Моя фамилия " + family + ". Мой возраст "
+//                + Age + ". Мой рост " + cheight + ". Мой вес " + weight);
+//            Console.WriteLine("Меня зовут {0}. Моя фамилия {1}. Мой возраст {2}. Мой рост {3}. Мой вес {4}", name, family, Age, cheight, weight);
+//            Console.WriteLine($"Меня зовут {name}. Моя фамилия {family}. Мой возраст {Age}. Мой рост {cheight}. Мой вес {weight}");
+
+//            Console.ReadKey(true);
+//        }
+//    }
+//}
+
+
+//class Human
+//   {
+//       static void Main()
+//       {
+//           int m = 85;
+//           double h = 1.73;
+//           double i = m / (h * h);
+//           Console.WriteLine("Вес равен " + i);
+//           Console.ReadKey(true);
+//       }
+//   }
+
+
+
+//class Rastoianie
+//{
+    
+//    static void Main()
+//    {
+//        int x1 = 3;
+//        int x2 = 4;
+//        int y1 = 5;
+//        int y2 = 6;
+
+//        double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+//            Console.WriteLine(r);
+//            Console.ReadKey(true);
+ 
+//    }
+//}
+
+
+//class Peremenie
+//{
+//    static void Main()
+//    {
+//        int a = 3;
+//        int b = 4;
+//        int c = a;
+//        a = b;
+//        b = c;
+
+//        Console.WriteLine("А равно " + a);
+//        Console.WriteLine("Б равно " + b);
+//        Console.ReadKey(true);
+
+//    }
+//}
+
+
+//class Peremenie
+//{
+//    static void Main()
+//    {
+//        int a = 3;
+//        int b = 4;
+
+//        a = a * b / b;
+//        b = a * b / a;
+//        Console.WriteLine("А равно " + a);
+//        Console.WriteLine("Б равно " + b);
+//        Console.ReadKey(true);
+//    }
+//}
+
+
+//    class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        string name = "Рамиль";
+//        string family = "Хайруллин";
+//        string sity = "Казань";
+
+//        Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+//        Console.WriteLine("Меня зовут " + name + " " + family  + " город проживания " +  sity);
+//        Console.ReadKey(true);
+//    }
+//}
+
+
+
+
